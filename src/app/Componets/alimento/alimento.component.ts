01b6bf2780cbc87c35effe8f7bd6ed931726a350
PauBrij <l.pauilita23@gmail.com>
2023-09-06 23:05:46 -0500

    Primero

---
+++ src/app/Componets/alimento/alimento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alimento',
+  templateUrl: './alimento.component.html',
+  styleUrls: ['./alimento.component.css']
+})
+export class AlimentoComponent {
+
+  title="Alimentos"
+}

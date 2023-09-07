1bfa40cdea33b180dee4a819d1e3a185bdaf64c5
PauBrij <l.pauilita23@gmail.com>
2023-09-06 23:07:18 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MascotaComponent } from './Componets/mascota/mascota.component';
+import { ClienteComponent } from './Componets/cliente/cliente.component';
+import { AlimentoComponent } from './Componets/alimento/alimento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Mascotas", component:MascotaComponent},
+  {path:"Cliente", component:ClienteComponent},
+  {path:"Alimento", component:AlimentoComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
